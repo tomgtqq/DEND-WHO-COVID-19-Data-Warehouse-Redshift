524c76f16f3bb8e0a691fa2488cea0363c8bdd1a
Tom <52593834@qq.com>
2021-03-08 15:14:46 +0800

    Feat: Query data from staging table and INSERT Fact and Dim tables

--- docker-airflow/plugins/operators/load_fact.py
+++ docker-airflow/plugins/operators/load_fact.py
@@ -28,10 +28,14 @@ class LoadFactOperator(BaseOperator):
         self.log.info(f"Create {self.table} fact tables")
         redshift_hook.run(f"{self.sql_create}")
         
+        columns = "(iso_code,date,vaccines_id,source_id,new_cases,cumulative_cases,new_deaths,cumulative_deaths,total_vaccinations,\
+                    people_vaccinated,people_fully_vaccinated,daily_vaccinations_raw,daily_vaccinations,total_vaccinations_per_hundred,\
+                    people_vaccinated_per_hundred,people_fully_vaccinated_per_hundred,daily_vaccinations_per_million)"
+        
         if self.mode == "delete-load":
             self.log.info(f"DELETE {self.table} fact tables")
             redshift_hook.run(f"DELETE FROM {self.table};")
             
         self.log.info(f"INSERT DATA INTO {self.table}")
-        redshift_hook.run(f"INSERT INTO {self.table} {self.sql_insert}")
+        redshift_hook.run(f"INSERT INTO {self.table} {columns} {self.sql_insert}")
         
